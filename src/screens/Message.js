8fc42633c50cfea13faebc377f4a5a337ead195c
penglangchen <penglangchen@gmail.com>
2018-04-14 16:24:10 -0400

    updated messages

--- src/screens/Message.js
+++ src/screens/Message.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, AsyncStorage } from 'react-native';
 import SmsAndroid  from 'react-native-get-sms-android';
 
 class CustomMessage extends Component {
@@ -18,8 +18,9 @@ export default class Message extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          text: 'Hey, this is Paul. I am in an emergency situation right now and I need help!'
-            + ' My current location is https://www.google.com/maps/search/?api=1&query=',
+          message: null,
+          message1: 'Hey, this is Paul. I am in an emergency situation right now and I need help!',
+          message2: null,
           latitude: null,
           longitude: null,
           error: null,
@@ -27,18 +28,46 @@ export default class Message extends Component {
     }
 
     componentDidMount() {
+        this.updateMessage1();
+        this.updateMessage2();
+    }
+
+    async updateMessage1() {
+        let response = await AsyncStorage.getItem('message1');
+        this.setState({
+            message1: response,
+        });
+    }    
+
+    async updateMessage2() {
         navigator.geolocation.getCurrentPosition(
-          (position) => {
-            this.setState({
-              text: this.state.text + position.coords.latitude + ',' + position.coords.longitude,
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude,
-              error: null,
-            });
-          },
-          (error) => this.setState({ error: error.message }),
-          { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+            (position) => {
+              this.setState({
+                message2: ' My current location is https://www.google.com/maps/search/?api=1&query=' +
+                    position.coords.latitude + ',' + position.coords.longitude,
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                error: null,
+              });
+            },
+            (error) => this.setState({ error: error.message }),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
         );
+    } 
+
+    async saveMessage() {
+        console.log("PRESSED");
+        AsyncStorage.setItem('message1', this.state.message1)
+        AsyncStorage.setItem('message', this.state.message1 + this.state.message2);
+        this.setState({
+            message: this.state.message1 + this.state.message2,
+        });
+
+        SmsAndroid.autoSend('6786779310', this.state.message1 + this.state.message2, (fail) => {
+            console.log("Failed with this error: " + fail)
+        }, (success) => {
+            console.log("SMS sent successfully");
+        });
     }
 
     render() {
@@ -47,25 +76,17 @@ export default class Message extends Component {
                 <CustomMessage
                     multiline = {true}
                     numberOfLines = {1}
-                    onChangeText={(text) => this.setState({text})}
-                    value={this.state.text}
+                    onChangeText={(message1) => this.setState({message1})}
+                    value={this.state.message1}
                 />
-                <Button onPress={this.save.bind(this)}
+                <Button onPress={this.saveMessage.bind(this)}
                     title="Save"
                     color="#25CCF7"
+                    borderBottomWidth="1"
                 />
+                <Text> Message: {this.state.message}</Text>
             </View>
         );
-    }
-
-    save(event) {
-        console.log("PRESSED");
-        SmsAndroid.autoSend('6786779310', this.state.text, (fail) => {
-            console.log("Failed with this error: " + fail)
-        }, (success) => {
-            console.log("SMS sent successfully");
-        });
-        console.log("PRESSED");
     }
 }
 
